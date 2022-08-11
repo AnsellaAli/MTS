1750feef9557eb887e40c2c04af50301451996c4
AnsellaAli <aansella@mail.ru>
2022-08-11 13:48:25 +0300

    mm

---
+++ src/test/java/pages/CardPage.java
@@ -0,0 +1,35 @@
+package pages;
+
+import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
+import org.openqa.selenium.By;
+
+import java.util.List;
+
+import static com.codeborne.selenide.CollectionCondition.*;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.$$;
+import static org.junit.Assert.assertTrue;
+
+public class CardPage {
+
+    private final List<SelenideElement> TAB_LIST = $$(".Wrapper-sc-1vydk7-0.jLWLXd.TabText-sc-tyqhzb-3.faRXmE");
+    private final By CARD_LIST = By.cssSelector(".Wrapper-sc-6nwvzq-0.kRJvZg.styled__TitleMobile-sc-m0n4zo-8.frcrJF");
+
+
+    @Step("Выбрать сортировку {0}")
+    public CardPage selectTab(String tabName) {
+        $$(TAB_LIST).find(text(tabName)).click();
+        return this;
+    }
+
+    @Step("Проверяем, что название карт содержит текст {0}")
+    public CardPage checkNamesCardsContainsText(String cardName) {
+       for (int i=0;i< $$(CARD_LIST).size(); i++){
+           String card = $$(CARD_LIST).get(i).getOwnText();
+            card.equals(cardName);
+        }
+        // $$(CARD_LIST).shouldHave(sizeGreaterThan(0)).stream().map(SelenideElement::getSelectedValue).forEach(name -> assertTrue(name.contains(cardName)));
+        return this;
+    }
+}
